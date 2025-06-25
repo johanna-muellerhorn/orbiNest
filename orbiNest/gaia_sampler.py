3a4dfb4d0f07c20614f7ac6c7f072d6cc9471a44
Müller-Horn <mueller-horn@mpia.de>
2025-06-25 22:00:42 +0200

    orbit fit with ruwe

--- orbiNest/gaia_sampler.py
+++ orbiNest/gaia_sampler.py
@@ -4,9 +4,77 @@ import os
 import numpy as np
 import ultranest
 import ultranest.stepsampler
+from scipy.optimize import fsolve
+
+from gaia_bin.forwardmodel import astrometric
 from .model import rv_model
-import gaiamock.gaiamock as gaiamock
-_C_FUNCS = gaiamock.read_in_C_functions()
+
+def binary_mass_function(m1=1., m2=1., period=1., K=1, e=0.,inc=np.pi/2, use_masses=False):
+    if use_masses:
+        binf = (m2*np.sin(inc))**3/(m1+m2)**2
+        return binf
+    else:
+        binf = (period*24*60*60)*(K*1e3)**3/(2*np.pi*6.67430e-11)*(1-e**2)**(3/2)
+        return binf/1.9884e30
+
+def solve_m1(m1, m0, f_M):
+    return (m1**3 / (m0 + m1)**2) - f_M
+
+# vectorized version for compatibility in likelihood
+def min_companion_mass(f_M_array, m0):
+    # Ensure inputs are numpy arrays
+    f_M_array = np.atleast_1d(f_M_array)
+
+    def solve_single(f_M_single):
+        return fsolve(solve_m1, 0.5, args=(m0, f_M_single))[0]
+
+    # Vectorize over the array of f_M values
+    m1_solutions = np.vectorize(solve_single)(f_M_array)
+
+    return m1_solutions
+
+def predict_ruwe(ra, dec, parallax, pmra, pmdec, m1, m2_array, P_array, Tp_array, e_array, omega_array, cosi_array, w_array,
+                 phot_g_mean_mag,f,t_ast_yr,psi,plx_factor,epoch_err_per_transit,data_release,bias_factor):
+    # Ensure inputs are numpy arrays
+    m2_array = np.atleast_1d(m2_array)
+    P_array = np.atleast_1d(P_array)
+    Tp_array = np.atleast_1d(Tp_array)
+    e_array = np.atleast_1d(e_array)
+    omega_array = np.atleast_1d(omega_array)
+    cosi_array = np.atleast_1d(cosi_array)
+    w_array = np.atleast_1d(w_array)
+
+    def solve_single(m2_single, P_single, Tp_single, e_single, omega_single, cosi_single, w_single):
+        return astrometric.predict_ruwe(
+            ra=ra,
+            dec=dec,
+            parallax=parallax,
+            pmra=pmra,
+            pmdec=pmdec,
+            m1=m1,
+            m2=m2_single,
+            period=P_single,
+            Tp=Tp_single,
+            ecc=e_single,
+            omega=omega_single,
+            inc=np.arccos(cosi_single),
+            w=w_single,
+            phot_g_mean_mag=phot_g_mean_mag,
+            f=f,
+            t_ast_yr=t_ast_yr,
+            psi=psi,
+            plx_factor=plx_factor,
+            epoch_err_per_transit=epoch_err_per_transit,
+            data_release=data_release,
+            bias_factor=bias_factor
+        )
+
+    # Vectorize over all input arrays
+    vectorized_ruwe = np.vectorize(solve_single)
+    ruwe_solutions = vectorized_ruwe(m2_array, P_array, Tp_array, e_array, omega_array, cosi_array, w_array)
+
+    return ruwe_solutions
+
 
 class OrbitalSamplerGaia:
     def __init__(self, target, hp, times, rvs, rvs_err,
@@ -86,7 +154,6 @@ class OrbitalSamplerGaia:
         e = np.abs(theta[:, [3]])
         w = theta[:, [4]]
         off = theta[:, [5]]
-        cosi = theta[:, [6]]
 
         t_rvs_day = self.target.t_ast_yr * 365.25 + gaia_ref # converting to modified julian dates
 
@@ -95,7 +162,6 @@ class OrbitalSamplerGaia:
 
         gaia_rvs = rv_model(theta, t_rvs_day)
 
-        Tp = tau*P+T_ref-gaia_ref
         pred_rv_amplitude_robust = (np.max(gaia_rvs,axis=1) - np.min(gaia_rvs,axis=1))*self.hp.bias_spectro
         pred_radial_velocity = (np.median(gaia_rvs,axis=1))
 
@@ -104,7 +170,7 @@ class OrbitalSamplerGaia:
         #print('RV mean and amplitude:', self.target.rv_gaia, self.target.rv_amplitude_gaia, gaia_rv_mean_pred, gaia_rv_amp_pred, loglike, loglike_gaia_mean, loglike_gaia_amp)
         return (loglike + loglike_gaia_mean + loglike_gaia_amp)
 
-    def log_likelihood_ruwe(self,theta):
+    def log_likelihood_ruwe(self,theta,T_ref=51544., gaia_ref=57388.5):
         # log-likelihood, for parameter vector theta=(logK, logf, mean anomaly, e, omega, jitter, offset)
         theta = np.atleast_2d(theta)
         model = rv_model(theta, self.times)
@@ -118,6 +184,7 @@ class OrbitalSamplerGaia:
         w = theta[:, [4]]
         off = theta[:, [5]]
         cosi = theta[:, [6]]
+        omega = theta[:, [7]]
 
         t_rvs_day = self.target.t_ast_yr * 365.25 + gaia_ref # converting to modified julian dates
 
@@ -126,7 +193,6 @@ class OrbitalSamplerGaia:
 
         gaia_rvs = rv_model(theta, t_rvs_day)
 
-        Tp = tau*P+T_ref-gaia_ref
         pred_rv_amplitude_robust = (np.max(gaia_rvs,axis=1) - np.min(gaia_rvs,axis=1))*self.hp.bias_spectro
         pred_radial_velocity = (np.median(gaia_rvs,axis=1))
 
@@ -138,26 +204,22 @@ class OrbitalSamplerGaia:
         Tp = tau*P+T_ref-gaia_ref
 
         fbin = binary_mass_function(K=K,period=P,e=e)
-        mcomp = np.maximum(min_companion_mass(fbin/np.sin(i)**3, self.target.mass), 0.01)
+        m2 = np.maximum(min_companion_mass(fbin/np.sin(np.arccos(cosi))**3, self.target.mass), 0.01)
         #print(fbin, mcomp)
-
-        astrometry = calculate_astrometry(self.target.ra_deg,self.target.dec_deg,self.target.parallax,self.target.GaiaG,self.target.pmra,self.target.pmdec,self.target.mass,
-                                           P, mcomp, e,i, Tp=Tp, omega=np.pi/2, w=w,f=f)
-
         # Predict astrometric observable (RUWE)
-        gaia_ruwe_pred = astrometric.predict_ruwe(ra=self.target.ra,
+        pred_ruwe = predict_ruwe(ra=self.target.ra,
                                     dec=self.target.dec,
                                     parallax=self.target.parallax,
                                     pmra=self.target.pmra,
                                     pmdec=self.target.pmdec,
                                     m1=self.target.mass,
-                                    m2=mcompanion,
-                                    period=period,
-                                    Tp=Tp,
-                                    ecc=ecc,
-                                    omega=omega,
-                                    inc=inc,
-                                    w=w,
+                                    m2_array=m2,
+                                    P_array=P,
+                                    Tp_array=Tp,
+                                    e_array=e,
+                                    omega_array=omega,
+                                    cosi_array=cosi,
+                                    w_array=w,
                                     phot_g_mean_mag=self.target.phot_g_mean_mag,
                                     f=self.hp.f,
                                     t_ast_yr=self.target.t_ast_yr,
@@ -168,8 +230,8 @@ class OrbitalSamplerGaia:
                                     bias_factor=self.hp.bias_astro)
 
         #loglike_gaia_ruwe  = - 1000. * int((ruwe_pred-self.target.ruwe)>self.target.ruwe_error)
-        loglike_gaia_ruwe  = - 0.5 * ((np.log(self.target.ruwe / gaia_ruwe_pred) / self.hp.eps_astro))**2
-        return (loglike + loglike_gaia_mean + loglike_gaia_amp+loglike_gaia_ruwe)
+        loglike_gaia_ruwe  = - 0.5 * ((np.log(self.target.ruwe / pred_ruwe) / self.hp.eps_astro))**2
+        return (loglike + loglike_gaia_mean + loglike_gaia_amp + loglike_gaia_ruwe.reshape(-1))
 
     def run(self, dlogz=0.01):
         self.result = self._sampler.run(min_num_live_points=self.nlive, frac_remain=dlogz)
